d4ed44058797f6a35fa795cc2f07c49ec17bec12
Derek Brown <djb3718@rit.edu>
2014-10-09 20:14:52 -0400

    Created java file for algorithm implementation

---
+++ CSCI261_hw3_p2_dynamic/src/LongestIncreasingSubseqDP.java
@@ -0,0 +1,14 @@
+/*
+ * LongestIncreasingSubswqDP.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu>
+ * 
+ * purpose	A dynamic programming implementation for finding the longest 
+ * 			subsequence of increasing numbers in an array.
+ */
+public class LongestIncreasingSubseqDP {
+
+	public static void main(String[] args) {
+
+	}//end main
+}//end LongestIncreasingSubseqDP
